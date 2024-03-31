5dc9c288941f938cc017a3eba8b12f510f5e6250
Sylvia <sylvia.tata@student.moringaschool.com>
2024-03-31 12:57:45 +0300

    basics of function declaration invocation and scope

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function expression as specified
+  let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define the wrapAdjective function according to the specification
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
